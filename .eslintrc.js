1d21e6db1f3c96d03fae64fa600aeb035d78e507
An hyunjin <dev.hyunjeen@gmail.com>
2023-07-10 21:04:35 +0900

    init: 마켓컬리 클론 프로젝트 with nextjs

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended",
+        "next/core-web-vitals",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script",
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+        "@typescript-eslint/explicit-function-return-type": ["off"],
+        "prettier/prettier": [
+            "error",
+            {
+                "endOfLine": "auto"
+            }
+        ],
+    }
+}
